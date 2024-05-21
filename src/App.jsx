69f389f781e364764925ea4925613c7fe4f4b1e4
Akinwumidi <israelakin14@gmail.com>
2024-05-21 19:10:32 +0100

    project initialization: Added tailwindcss and shadcn UI

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+import './App.css'
+
+function App() { 
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      <Button className="mt-10">Schand UI Button</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
